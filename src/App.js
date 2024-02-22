cb942fa3d29fd4a378d2d8ca3df71dd0298029b7
SafarzadeJunayd <safarzadejunayd@gmail.com>
2024-02-22 23:52:40 +0300

    cloned master

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css'
+import {Main, Info, Portfolio, Services, Prices, Quote, QnA, Contacts} from './container'
+import './assets/fonts/RozoviiChulok.ttf'
+const App = () => {
+    return (
+        <div className='app'>
+            <Main/>
+            <Info/>
+            <Portfolio/>
+            <Services/>
+            <Prices/>
+            <Quote/>
+            <QnA/>
+            <Contacts/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/container/Quote/Quote.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Quote.css';
+
+const Quote = () => (
+    <div className='quote'>
+        <div className='quote__text'>
+            " Покажу твою красоту й чувственность через<br/>свои взгляд"
+        </div>
+    </div>
+);
+
+export default Quote;
