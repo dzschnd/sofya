afe68135a50b94c04e24ed4a5d2f15e0652539de
SafarzadeJunayd <safarzadejunayd@gmail.com>
2024-02-27 00:57:21 +0300

    clumsily made services block

--- src/container/Portfolio/Portfolio.jsx
+++ src/container/Portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Portfolio.css';
+import './Portfolio.scss';
 
 const Portfolio = () => (
     <div className='portfolio'>
@@ -10,33 +10,15 @@ const Portfolio = () => (
                     Портфолйо
                 </div>
                 <div className='portfolio__body'>
-                    <div className='image'>
-                        <img src={require('../../assets/images/portfolio/image-1.png')} alt=''/>
-                    </div>
-                    <div className='image'>
-                        <img src={require('../../assets/images/portfolio/image-2.png')} alt=''/>
-                    </div>
-                    <div className='image'>
-                        <img src={require('../../assets/images/portfolio/image-3.png')} alt=''/>
-                    </div>
-                    <div className='image'>
-                        <img src={require('../../assets/images/portfolio/image-4.png')} alt=''/>
-                    </div>
-                    <div className='image'>
-                        <img src={require('../../assets/images/portfolio/image-5.png')} alt=''/>
-                    </div>
-                    <div className='image'>
-                        <img src={require('../../assets/images/portfolio/image-6.png')} alt=''/>
-                    </div>
-                    <div className='image'>
-                        <img src={require('../../assets/images/portfolio/image-7.png')} alt=''/>
-                    </div>
-                    <div className='image'>
-                        <img src={require('../../assets/images/portfolio/image-8.png')} alt=''/>
-                    </div>
-                    <div className='image' id='last'>
-                        <img src={require('../../assets/images/portfolio/image-9.png')} alt=''/>
-                    </div>
+                        <img className="portfolio__image-1" src={require('../../assets/images/portfolio/black-n-white/image-1.png')} alt=''/>
+                        <img className="portfolio__image-2" src={require('../../assets/images/portfolio/black-n-white/image-2.png')} alt=''/>
+                        <img className="portfolio__image-3" src={require('../../assets/images/portfolio/black-n-white/image-3.png')} alt=''/>
+                        <img className="portfolio__image-4" src={require('../../assets/images/portfolio/black-n-white/image-4.png')} alt=''/>
+                        <img className="portfolio__image-5" src={require('../../assets/images/portfolio/black-n-white/image-5.png')} alt=''/>
+                        <img className="portfolio__image-6" src={require('../../assets/images/portfolio/black-n-white/image-6.png')} alt=''/>
+                        <img className="portfolio__image-7" src={require('../../assets/images/portfolio/black-n-white/image-7.png')} alt=''/>
+                        <img className="portfolio__image-8" src={require('../../assets/images/portfolio/black-n-white/image-8.png')} alt=''/>
+                        <img className="portfolio__image-9" src={require('../../assets/images/portfolio/black-n-white/image-9.png')} alt=''/>
                 </div>
             </div>
         </div>
--- src/container/Services/Services.jsx
+++ src/container/Services/Services.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 
-import './Sevices.css';
+import './Sevices.scss';
 
 const Services = () => (
     <div className='services'>
+        <div className='services__card'>
         <div className='services__content'>
             <div className='services__header'>Услугй</div>
             <div className='services__body'>
                 <div className='services__image'>
-                    <img src={require('../../assets/images/services/image-individual.png')}/>
-                    <span>Индйвйдуальная съемка</span>
+                    <img className='services__image-1' src={require('../../assets/images/services/black-n-white/image-individual.png')}/>
+                    <span>Индйвйдуальная<br/>съемка</span>
                 </div>
                 <div className='services__image'>
-                    <img src={require('../../assets/images/services/image-katzen.png')}/>
-                    <span>Парная съемка</span>
+                    <img className='services__image-2' src={require('../../assets/images/services/black-n-white/image-katzen.png')}/>
+                    <span>Парная<br id='adapting'/> съемка</span>
                 </div>
                 <div className='services__image'>
-                    <img src={require('../../assets/images/services/image-family.png')}/>
-                    <span>Семеиная съемка</span>
+                    <img className='services__image-3' src={require('../../assets/images/services/black-n-white/image-family.png')}/>
+                    <span>Семеиная<br id='adapting'/> съемка</span>
                 </div>
                 <div className='services__image'>
-                    <img src={require('../../assets/images/services/image-content.png')}/>
-                    <span>Контент-съемка</span>
+                    <img className='services__image-4' src={require('../../assets/images/services/black-n-white/image-content.png')}/>
+                    <span>Контент-<br id='adapting'/>съемка</span>
                 </div>
             </div>
+        </div>
         </div>
     </div>
 );
