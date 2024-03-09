b4283eacc5cd35454d92c01c03ce4343009cb4f2
SafarzadeJunayd <safarzadejunayd@gmail.com>
2024-03-10 02:27:06 +0300

    save before a reset

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import './App.css'
 import {Main, Info, Portfolio, Services, Prices, Quote, QnA, Contacts} from './container'
 import './assets/fonts/RozoviiChulok.ttf'
+import Infomur from "./container/Info/Info-mur";
 
 const App = () => {
     return (
         <div className='app'>
             {/*<Main/>*/}
-            <Info/>
+            {/*<Infomur/>*/}
             {/*<Portfolio/>*/}
             {/*<Services/>*/}
             {/*<Prices/>*/}
